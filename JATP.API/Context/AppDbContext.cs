805a0cc5cdea703de24d41ad2ca69c0ae13d4df9
Ata633 <100758201+Ata633@users.noreply.github.com>
2023-08-20 15:12:56 +0500

    Create user models, DbContext and SqlServerDb then connect with database

---
+++ JATP.API/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using JATP.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JATP.API.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+}
