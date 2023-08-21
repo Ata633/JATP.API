44c6849d7bde4120a4a15230a2376df6b14e43ba
Ata633 <100758201+Ata633@users.noreply.github.com>
2023-08-21 10:58:38 +0500

    Password Hashing in .Net

--- JATP.API/Controllers/UserController.cs
+++ JATP.API/Controllers/UserController.cs
@@ -1,8 +1,11 @@
 ﻿using JATP.API.Context;
+using JATP.API.Helpers;
 using JATP.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace JATP.API.Controllers
 {
@@ -41,10 +44,25 @@ namespace JATP.API.Controllers
         public async Task<IActionResult> RegisterUser([FromBody] User userObj)
         {
             if (userObj == null)
-            {
                 return BadRequest();
-            }
 
+            // Check username
+            if(await CheckUserNameExistAsync(userObj.Username))
+                return BadRequest(new { Message = "Username Already Exist!" });
+                
+
+            // Check email
+            if (await CheckEmailExistAsync(userObj.Email))
+                return BadRequest(new { Message = "Email Already Exist!" });
+
+            // Check password Strength
+            var pass = CheckPasswordStrength(userObj.Password);
+            if(!string.IsNullOrEmpty(pass))
+                return BadRequest(new { Message = pass.ToString() });
+
+            userObj.Password = PasswordHasher.HashPassword(userObj.Password);
+            userObj.Role = "User";
+            userObj.Token = "";
             await _authContext.Users.AddAsync(userObj);
             await _authContext.SaveChangesAsync();
 
@@ -52,6 +70,26 @@ namespace JATP.API.Controllers
             {
                 Message = "User Registered!"
             });
+        }
+
+        private Task<bool> CheckUserNameExistAsync(string username)
+            => _authContext.Users.AnyAsync(x => x.Username == username);
+
+
+        private Task<bool> CheckEmailExistAsync(string email)
+            => _authContext.Users.AnyAsync(x => x.Email == email);
+
+        private string CheckPasswordStrength(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            if(password.Length < 8)
+                sb.Append("Minimum password length shoud be 8" + Environment.NewLine);
+            if (!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]") && Regex.IsMatch(password, "[0-9]")))
+                sb.Append("Password shoud be Alphanumeric" + Environment.NewLine);
+            if (!Regex.IsMatch(password, "[!,”,#,$,%,&,’,(,),*,+,-,.,/,:,;,<,=,>,?,@,[,\\,^,_,`,{,|,},~,]"))
+                sb.Append("Password should contain special chars" + Environment.NewLine);
+
+            return sb.ToString();
         }
     }
 }
